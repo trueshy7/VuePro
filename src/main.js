7572ff67ce83df45bc324dfe379ff976d1fe9ab9
trueshy7 <3421970295@qq.com>
2025-06-06 11:13:34 +0800

    login完成

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'element-plus/dist/index.css'
+import axios from 'axios';
 import App from './App.vue'
 
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
 }
+axios.defaults.baseURL = 'http://localhost:80'
+app.config.globalProperties.$axios = axios
 app.use(ElementPlus)
 app.mount('#app')
